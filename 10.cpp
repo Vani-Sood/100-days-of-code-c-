3c5f0698c045d574a8977d858010b8eb675e1faf
Vani-Sood <119438297+Vani-Sood@users.noreply.github.com>
2026-02-04 23:51:34 +0530

    Add files via upload

---
+++ 10.cpp
@@ -0,0 +1,10 @@
+//wap to display numbers in hexadecimal form
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	cout<<"The hexadecimal form is: "<<hex<<n<<endl;//lowercase
+	cout << "Hexadecimal form: " << uppercase << hex << n;
+	return 0;
+} 
