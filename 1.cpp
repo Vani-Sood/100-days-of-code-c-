a08b70ca8e294b92cba34a71728a0c6d2ba4ccd0
Vani-Sood <119438297+Vani-Sood@users.noreply.github.com>
2026-02-04 23:20:31 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,10 @@
+//wap to print your name, section
+#include<iostream>
+using namespace std;
+int main(){
+	string name;
+	string section;
+	cin>>name>>section;
+	cout<<"My name is: "<<name<<" and my section is: "<<section;
+	return 0;
+}
